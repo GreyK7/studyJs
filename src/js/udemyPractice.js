9aee9a5d748af112a9fed3e63bcb1c38b0e5b3cb
Grey <criro666@gmail.com>
2023-09-07 14:46:51 +0300

    Second udemy practice

--- src/js/udemyPractice.js
+++ src/js/udemyPractice.js
@@ -1,8 +1,8 @@
 'use strict';
 
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '0');
-
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+console.log(numberOfFilms);
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -13,9 +13,23 @@ let personalMovieDB = {
 };
 
 for (let i = 0; i < 2; i++) {
-    let movieName = prompt('Один из последних просмотренных фильмов?', '');
-    let movieRating = prompt('На сколько оцените его?', '');
-    personalMovieDB["movies"][movieName] = movieRating;
+    let movieName = prompt('Один из последних просмотренных фильмов?', '')?.trim();
+    let movieRating = prompt('На сколько оцените его?', '')?.trim();
+    if ((movieName === undefined || movieName === ''  || movieName.length > 50) ||  ( movieRating === undefined || movieRating === '')) {
+        i--;
+    } else {
+        personalMovieDB["movies"][movieName] = movieRating;
+    }
+}
+
+if (numberOfFilms < 10) {
+    console.log('Просмотренно довольно мало фильмаов');
+} else if (numberOfFilms < 30 ) {
+    console.log('Вы классический зритель');
+} else if (numberOfFilms >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
 }
 
 console.log(personalMovieDB);
